9df0cf87368a08a51a54e65a5e34f4f117c1f32e
Vadim-Li-V <vadker4@gmail.com>
2024-10-23 23:44:53 +0700

    Commit

---
+++ app/src/main/java/ru/yandex/practicum/contacts/presentation/base/ListDiffInterface.java
@@ -0,0 +1,7 @@
+package ru.yandex.practicum.contacts.presentation.base;
+
+public interface ListDiffInterface<T> {
+    boolean theSameAs(T other);
+    @Override
+    boolean equals(Object obj);
+}
